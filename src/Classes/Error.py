9fd06bce6d477b301eebe36ea0d91caa1dbfe040
Adam Ližičiar <xlizic00@vutbr.cz>
2024-02-09 17:42:33 +0100

    Error class

---
+++ src/Classes/Error.py
@@ -0,0 +1,24 @@
+#############################################
+# File:		Error file						#
+# Author: 	Adam Ližičiar (xlizic00)		#
+# Date: 	9.2.2024						#
+#############################################
+
+import sys
+
+class Error:
+    def __init__(self):
+        self.errors = {
+            10: "missing script paramter or using prohibited combination of parameters",
+            11: "problem with opening input file",
+            12: "problem when opening output file",
+            21: "wrong or missing header in the source code",
+            22: "unknown or incorrect operation code in the course code",
+            23: "other lexical or syntactic error of the source code",
+            99: "internal error"
+        }
+
+    def exit_program_with_err_msg(self, errNum):
+        print("Error: " + self.errors[errNum] + "!", file=sys.stderr)
+        sys.exit(errNum)
+
---
+++ src/parse.py
@@ -0,0 +1,18 @@
+#############################################
+# File:		Main file						#
+# Author: 	Adam Ližičiar (xlizic00)		#
+# Date: 	9.2.2024						#
+#############################################
+
+import sys
+from Classes.Error import Error
+
+# Init classes
+err = Error()
+
+#############################################
+
+# todo: Parse arguments
+# todo: Lopp throug input
+# todo: Generate xml
+
